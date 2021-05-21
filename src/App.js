1b48bc4c9e11e11dc78a1c2173f843cc436043c7
RahulAnand442001 <sahurahul442001@gmail.com>
2021-05-21 22:29:48 +0530

    builded netflix-clone web app with featured UI and functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { requests } from "./requests";
+
+// stylesheets
+import "./App.css";
+
+// custom components
+import Row from "./components/row/row.component.jsx";
+import Banner from "./components/banner/banner.component";
+import Navbar from "./components/navbar/navbar.component";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Navbar />
+			<Banner />
+			<>
+				<Row
+					title="NETFLIX ORIGINALS"
+					fetchUrl={requests.fetchNetflixOriginals}
+					isLargeRow
+				/>
+				<Row
+					title="Trending Now"
+					fetchUrl={requests.fetchTrending}
+				/>
+				<Row
+					title="Top Rated"
+					fetchUrl={requests.fetchTopRated}
+				/>
+				<Row
+					title="Action Movies"
+					fetchUrl={requests.fetchActionMovies}
+				/>
+				<Row
+					title="Comedy Movies"
+					fetchUrl={requests.fetchComedyMovies}
+				/>
+				<Row
+					title="Horror Movies"
+					fetchUrl={requests.fetchHorrorMovies}
+				/>
+				<Row
+					title="Romantic Movies"
+					fetchUrl={requests.fetchRomanceMovies}
+				/>
+				<Row
+					title="Documentaries"
+					fetchUrl={requests.fetchDocumentaries}
+				/>
+			</>
+		</div>
+	);
 }
 
 export default App;
