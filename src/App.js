e1cdda287e4882eba6960025507e18f318d9247f
RahulAnand442001 <sahurahul442001@gmail.com>
2021-05-22 16:04:17 +0530

    refactored application with react-router-dom, add home component, might add later

--- src/App.js
+++ src/App.js
@@ -1,53 +1,27 @@
-import { requests } from "./requests";
+import React from "react";
+import {
+	BrowserRouter,
+	Switch,
+	Route,
+} from "react-router-dom";
 
 // stylesheets
 import "./App.css";
 
 // custom components
-import Row from "./components/row/row.component.jsx";
-import Banner from "./components/banner/banner.component";
+
 import Navbar from "./components/navbar/navbar.component";
+import MovieGallery from "./components/movie-gallery/movie-gallery.component";
 
 function App() {
 	return (
 		<div className="app">
 			<Navbar />
-			<Banner />
-			<>
-				<Row
-					title="NETFLIX ORIGINALS"
-					fetchUrl={requests.fetchNetflixOriginals}
-					isLargeRow
-				/>
-				<Row
-					title="Trending Now"
-					fetchUrl={requests.fetchTrending}
-				/>
-				<Row
-					title="Top Rated"
-					fetchUrl={requests.fetchTopRated}
-				/>
-				<Row
-					title="Action Movies"
-					fetchUrl={requests.fetchActionMovies}
-				/>
-				<Row
-					title="Comedy Movies"
-					fetchUrl={requests.fetchComedyMovies}
-				/>
-				<Row
-					title="Horror Movies"
-					fetchUrl={requests.fetchHorrorMovies}
-				/>
-				<Row
-					title="Romantic Movies"
-					fetchUrl={requests.fetchRomanceMovies}
-				/>
-				<Row
-					title="Documentaries"
-					fetchUrl={requests.fetchDocumentaries}
-				/>
-			</>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={MovieGallery} />
+				</Switch>
+			</BrowserRouter>
 		</div>
 	);
 }
